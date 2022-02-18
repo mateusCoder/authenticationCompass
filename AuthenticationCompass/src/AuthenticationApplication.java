6a25e9af014868ee0269596bfdd6748eb4ea7a4e
mateusCoder <mateus.moraes0507@gmail.com>
2022-02-18 00:32:21 -0300

    Add Class User

--- AuthenticationCompass/src/AuthenticationApplication.java
+++ AuthenticationCompass/src/AuthenticationApplication.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class AuthenticationApplication {
 	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
 		String user;
 		String key;
-		Scanner read = new Scanner(System.in);
 		
 		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH");
 		String hourNow = formatterHora.format(LocalDateTime.now());
 		int hourNowInt = Integer.parseInt(hourNow);
 	
-		User account = new User();
-		
+		User account = new User("fernanda", "0123");
+		User account2 = new User("mateus", "54321");
+		User account3 = new User("xablau", "54321");
+				
 		System.out.println("usuário: ");
 		user = read.next();
 		System.out.println("senha: ");
 		key = read.next();
-				
-		System.out.println();
 		
 		if(account.autenthicationUser(user, key)) {
-			if(hourNowInt > 0 && hourNowInt < 6) {
+			if(hourNowInt >= 0 && hourNowInt < 6) {
 				System.out.println("Boa madrugada, você se logou ao nosso sistema.");
 			}else if(hourNowInt >= 6 && hourNowInt < 12) {
 				System.out.println("Bom dia, você se logou ao nosso sistema.");
--- AuthenticationCompass/src/User.java
+++ AuthenticationCompass/src/User.java
@@ -1,18 +1,23 @@
+import java.util.ArrayList;
 
 public class User{
-	private String user;
-	private String key;
+	private static ArrayList<String> users = new ArrayList<>();
+	private static ArrayList<String> keys = new ArrayList<>();
 	
-//	public String getUser() {
-//		return this.user;
-//	}
-	
-	public boolean autenthicationUser(String user, String key) {
-		if(user.equals("mateus") && key.equals("12345")){
-			return true;
+	public User(String users, String keys) {
+		this.users.add(users);
+		this.keys.add(keys);
+	}
+
+	public boolean autenthicationUser(String user, String key) {	
+		for(int i = 0; i < users.size(); i++) {
+			if(user.equals(users.get(i)) && key.equals(keys.get(i))){
+				return true;
+			}
 		}
 		return false;
 	}
+	
 }
 
 
